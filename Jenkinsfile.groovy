cbf9239828b21d70e49e729d02c8dac52ff19180
raveel12 <162273836+raveel12@users.noreply.github.com>
2024-05-10 22:35:57 +1000

    added "Deployed"

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -105,6 +105,7 @@ pipeline {
         stage('Deploy To Production') {
             steps {
                 echo "Deploy the application to AWS EC2 instance"
+                echo "Deployed"
                 
             }
         }  
